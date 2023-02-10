5a945dd243aabac9b06e4c5ea9cf168e4bc52ad9
Aparajita <aparajita@aparajita.com>
2023-02-09 17:03:46 -0800

    feat: updates for capacitor-dark-mode 3.2

--- src/components/DarkModeDemo.vue
+++ src/components/DarkModeDemo.vue
@@ -1,88 +1,121 @@
 <template>
-  <ion-list>
-    <ion-radio-group
-      v-model="appearance"
-      @ion-change="updateAppearance"
-    >
-      <ion-list-header>
-        <ion-label>Appearance</ion-label>
-      </ion-list-header>
-
+  <ion-content
+    class="ion-padding-vertical"
+    :fullscreen="true"
+  >
+    <ion-list>
       <ion-item>
-        <ion-label>System</ion-label>
-        <ion-radio
-          slot="start"
-          :value="DarkModeAppearance.system"
-        ></ion-radio>
+        <ion-label>Appearance:</ion-label>
+        <ion-select
+          v-model="appearance"
+          interface="popover"
+          @ion-change="updateAppearance"
+        >
+          <ion-select-option :value="DarkModeAppearance.system">
+            System
+          </ion-select-option>
+          >
+          <ion-select-option :value="DarkModeAppearance.light">
+            Light
+          </ion-select-option>
+          >
+          <ion-select-option :value="DarkModeAppearance.dark">
+            Dark
+          </ion-select-option>
+          >
+        </ion-select>
       </ion-item>
 
       <ion-item>
-        <ion-label>Light</ion-label>
-        <ion-radio
-          slot="start"
-          :value="DarkModeAppearance.light"
-        ></ion-radio>
+        <ion-checkbox
+          v-model="disableTransitions"
+          @ion-change="onConfigChange"
+        />
+        <ion-label>Disable transitions</ion-label>
       </ion-item>
 
-      <ion-item>
-        <ion-label>Dark</ion-label>
-        <ion-radio
-          slot="start"
-          :value="DarkModeAppearance.dark"
-        ></ion-radio>
-      </ion-item>
-    </ion-radio-group>
+      <div v-if="isAndroid">
+        <ion-item>
+          <ion-label>Sync status bar</ion-label>
+          <ion-select
+            v-model="syncStatusBar"
+            interface="popover"
+            @ion-change="onConfigChange"
+          >
+            <ion-select-option :value="true"> Yes </ion-select-option>
+            <ion-select-option :value="false"> No </ion-select-option>
+            <ion-select-option value="textOnly"> Text only </ion-select-option>
+          </ion-select>
+        </ion-item>
 
-    <ion-radio-group
-      v-if="isAndroid"
-      v-model="syncStatusBar"
-      @ion-change="onSyncStatusBarChange"
-    >
-      <ion-list-header>
-        <ion-label>Sync status bar</ion-label>
-      </ion-list-header>
+        <div v-if="syncStatusBar">
+          <ion-item>
+            <ion-checkbox
+              v-model="useCustomStatusBarBackground"
+              @ion-change="onConfigChange"
+            />
+            <ion-label>Custom status bar background</ion-label>
+          </ion-item>
 
-      <ion-item>
-        <ion-label>Yes</ion-label>
-        <ion-radio
-          slot="start"
-          :value="true"
-        ></ion-radio>
-      </ion-item>
+          <div v-if="useCustomStatusBarBackground">
+            <ion-item>
+              <ion-checkbox
+                v-model="useCustomStyleGetter"
+                @ion-change="DarkMode.update()"
+              />
+              <ion-label>Custom style getter (reverses)</ion-label>
+            </ion-item>
 
-      <ion-item>
-        <ion-label>No</ion-label>
-        <ion-radio
-          slot="start"
-          :value="false"
-        ></ion-radio>
-      </ion-item>
+            <ion-item>
+              <ion-label>Light status bar color:</ion-label>
+              <ion-input
+                v-model="lightStatusBarColor"
+                @ion-change="onColorChange(lightStatusBarColor)"
+              ></ion-input>
+            </ion-item>
 
-      <ion-item>
-        <ion-label>Text only</ion-label>
-        <ion-radio
-          slot="start"
-          value="textOnly"
-        ></ion-radio>
-      </ion-item>
-    </ion-radio-group>
-  </ion-list>
+            <ion-item>
+              <ion-label>Dark status bar color:</ion-label>
+              <ion-input
+                v-model="darkStatusBarColor"
+                @ion-change="onColorChange(darkStatusBarColor)"
+              ></ion-input>
+            </ion-item>
+          </div>
+        </div>
+      </div>
+    </ion-list>
+  </ion-content>
 </template>
 
 <script setup lang="ts">
-import type { DarkModeListenerHandle } from '@aparajita/capacitor-dark-mode'
-import { DarkMode, DarkModeAppearance } from '@aparajita/capacitor-dark-mode'
+import type {
+  DarkModeListenerData,
+  DarkModeListenerHandle,
+  DarkModeOptions,
+  StatusBarStyleGetterResult
+} from '@aparajita/capacitor-dark-mode'
+import {
+  appearanceToStyle,
+  DarkMode,
+  DarkModeAppearance,
+  isDarkColor,
+  isValidHexColor
+} from '@aparajita/capacitor-dark-mode'
 import { Capacitor } from '@capacitor/core'
+import { Style, StatusBar } from '@capacitor/status-bar'
 import {
   alertController,
+  IonCheckbox,
+  IonContent,
+  IonInput,
   IonItem,
   IonLabel,
   IonList,
-  IonListHeader,
-  IonRadio,
-  IonRadioGroup
+  IonSelect,
+  IonSelectOption
 } from '@ionic/vue'
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue'
 import {
   getAppearancePref,
   getSyncStatusBarPref,
@@ -90,9 +123,17 @@ import {
   setSyncStatusBarPref
 } from '@/prefs'
 
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 const isAndroid = Capacitor.getPlatform() === 'android'
 const syncStatusBar = ref(getSyncStatusBarPref())
-const appearance = ref(DarkModeAppearance.system)
+const lightStatusBarColor = ref('#011c55')
+const darkStatusBarColor = ref('#ccffc0')
+const appearance = ref<DarkModeAppearance>(DarkModeAppearance.system)
+const disableTransitions = ref(true)
+const isDark = ref(false)
+const useCustomStatusBarBackground = ref(false)
+const useCustomStyleGetter = ref(false)
+const kStatusBarBackgroundVariable = '--statusBarBackground'
 let appearanceListenerHandle: DarkModeListenerHandle | null = null
 
 async function showAlert(message: string): Promise<void> {
@@ -105,12 +146,36 @@ async function showAlert(message: string): Promise<void> {
   await alert.present()
 }
 
+function getDarkModeConfig(): DarkModeOptions {
+  return {
+    syncStatusBar: syncStatusBar.value,
+    statusBarBackgroundVariable: useCustomStatusBarBackground.value
+      ? kStatusBarBackgroundVariable
+      : '',
+    statusBarStyleGetter: statusBarStyle,
+    disableTransitions: disableTransitions.value
+  }
+}
+
+onBeforeMount(async () => {
+  isDark.value = (await DarkMode.isDarkMode()).dark
+  await DarkMode.configure(getDarkModeConfig())
+})
+
 onMounted(async () => {
   appearanceListenerHandle = await DarkMode.addAppearanceListener(
-    ({ dark }) => {
-      showAlert(`System dark mode is ${dark ? 'on' : 'off'}.`).catch(
-        console.error
-      )
+    (data: DarkModeListenerData) => {
+      // eslint-disable-next-line @typescript-eslint/padding-line-between-statements
+      ;(async (): Promise<void> => {
+        isDark.value = data.dark
+        const style = statusBarStyle(appearanceToStyle(appearance.value))
+
+        if (style) {
+          await StatusBar.setStyle({ style })
+        }
+
+        await showAlert(`System dark mode is ${data.dark ? 'on' : 'off'}.`)
+      })()
     }
   )
 
@@ -127,9 +192,32 @@ onUnmounted(() => {
   appearanceListenerHandle?.remove()
 })
 
-async function onSyncStatusBarChange(): Promise<void> {
+function statusBarStyle(
+  style?: Style,
+  color?: string
+): StatusBarStyleGetterResult {
+  if (color && useCustomStatusBarBackground.value) {
+    const dark = isDarkColor(color)
+
+    if (useCustomStyleGetter.value) {
+      return dark ? Style.Light : Style.Dark
+    }
+
+    return dark ? Style.Dark : Style.Light
+  }
+
+  return null
+}
+
+async function onColorChange(color: string): Promise<void> {
+  if (useCustomStatusBarBackground.value && isValidHexColor(color)) {
+    await DarkMode.update()
+  }
+}
+
+async function onConfigChange(): Promise<void> {
   setSyncStatusBarPref(syncStatusBar.value)
-  await DarkMode.init({ syncStatusBar: syncStatusBar.value })
+  await DarkMode.configure(getDarkModeConfig())
 }
 
 async function updateAppearance(): Promise<void> {
@@ -137,3 +225,13 @@ async function updateAppearance(): Promise<void> {
   await DarkMode.update()
 }
 </script>
+
+<style>
+ion-content {
+  --statusBarBackground: v-bind(lightStatusBarColor);
+}
+
+body.dark ion-content {
+  --statusBarBackground: v-bind(darkStatusBarColor);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,22 +5,11 @@
         <ion-title>Dark mode demo</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content
-      class="ion-padding-vertical"
-      :fullscreen="true"
-    >
-      <DarkModeDemo />
-    </ion-content>
+    <DarkModeDemo />
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
-} from '@ionic/vue'
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'
 import DarkModeDemo from '@/components/DarkModeDemo.vue'
 </script>
