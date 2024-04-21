d1c3b53701020fe6e7cd28a395485702c7578766
Aparajita <aparajita@aparajita.com>
2024-04-20 19:44:06 -0700

    chore: update linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,16 @@ module.exports = {
   rules: {
     '@typescript-eslint/naming-convention': 'off',
   },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  overrides: [
+    {
+      // Module js
+      files: ['*.mjs'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
 }
